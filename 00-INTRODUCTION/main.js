bb4d9f091b3a0752fd9bf846a37d10a8dd1c19d3
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2022-03-09 11:33:52 +0100

    ajout du repertoire 00

---
+++ 00-INTRODUCTION/main.js
@@ -0,0 +1,8 @@
+// Crée par Joachim Zadi le 09/03/2022 à 11:08. Version 1.0
+// ========================================================
+
+console.log(2 + 3); // Une instruction JS ==> Elle se termine par un point virgule
+
+// Pour executer le JS dans la console, on utiliseras nodejs
+
+console.log("Mon premier fichier JavaScript depuis le fichier main.js");
