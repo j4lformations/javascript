4887a616411fc23b104c398648927fde9d9a15fe
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2022-03-14 11:39:35 +0100

    ajout du repertoire 07 exo_8-1

--- 07-EXERCICES/assets/js/exo_huit.js
+++ 07-EXERCICES/assets/js/exo_huit.js
@@ -1,2 +1,28 @@
 // Crée par Joachim Zadi le 14/03/2022 à 11:04. Version 1.0
 // ========================================================
+
+// EXERCICE 08
+// 1. Lire 10 nombres et trouver le plus grand.
+
+let plusGrand = -Infinity;
+let chaine = '';
+for (let i = 1; i < 11; i++) {
+    let saisie = Number(prompt(`Saisie N°${i}`));
+    if (isNaN(saisie)) {
+        do {
+            alert("Vous devez saisir un nombre !!!");
+            saisie = Number(prompt(`Saisie N°${i}`));
+        } while (isNaN(saisie));
+    } else {
+        chaine += `${saisie} `;
+        if (saisie > plusGrand) {
+            plusGrand = saisie;
+        }
+    }
+}
+console.log(chaine);
+console.log(`plusGrand = ${plusGrand}`);
+
+
+// 2. Lire 10 nombres et trouver le plus petit et son rang.
+// 3. Lire n nombres et en calculer la moyenne.
