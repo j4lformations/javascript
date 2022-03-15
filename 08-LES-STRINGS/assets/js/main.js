2ca1edaeb4eee8bf6455e14d90e21e2a14ee8ccf
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2022-03-15 18:14:31 +0100

    maj 2 du repertoire 08

--- 08-LES-STRINGS/assets/js/main.js
+++ 08-LES-STRINGS/assets/js/main.js
@@ -20,4 +20,25 @@ console.log('dragon rouge'.toUpperCase());
 maChaine = "KFC,McDO,BK,Grec";
 console.log(maChaine.split(','));
 console.log('dwwm'.split(""));
-console.log('Je suis candidat à la formation dwwm'.replace('dwwm','web-design'));
+console.log('Je suis candidat à la formation dwwm'.replace('dwwm', 'web-design'));
+
+
+// CONVERSION DE TYPE
+// ------------------
+//
+// Une chaine de caracteres peut etre creee a partir de n'importe quel objet JavaScript.
+// On utilise pour cela la methode toString()
+
+let tab = [1, 2, 3, 4, 5];
+console.log(tab.toString());
+
+let nb = -15.45;
+console.log(nb.toString());
+
+// NETTOYER UNE CHAINE
+// ------------------
+let maChaine = ' Voici mon texte ! ';
+console.log(maChaine.length);
+console.log(maChaine);
+console.log(maChaine.trim());
+
