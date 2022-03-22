b53ab2cbe6b4d40ddf7e495c2985d74387cdea43
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2022-03-22 11:04:15 +0100

    maj du repertoire 18-DOM-LES-FORMS

---
+++ 18-DOM-LES-FORMS/assets/js/main.js
@@ -0,0 +1,35 @@
+// Crée par Joachim Zadi le 21/03/2022 à 15:03. Version 1.0
+// ========================================================
+
+// =================
+// LES FORMULAIRES =
+// =================
+
+// console.log(document.getElementById('monForm'));
+// console.log(document.appForm);
+// console.log(document.forms[0]);
+// console.log(document.forms['appForm']);
+
+let monForm = document.appForm;
+let saisie = document.getElementById('saisie');
+// console.log(saisie.value);
+
+
+monForm.addEventListener('submit', soumission);
+
+function soumission(e) {
+    // let affichage = document.getElementById('affichage');
+    // affichage.innerText = `Vous avez saisie ${saisie.value}`;
+
+    if (document.getElementById('affichage')) {
+        document.body.removeChild(document.getElementById('affichage'));
+    }
+
+    let divAffichage = document.createElement('div');
+    divAffichage.setAttribute('id', 'affichage');
+    divAffichage.innerText = `Vous avez saisie ${saisie.value}`;
+    document.body.appendChild(divAffichage);
+
+    e.preventDefault();
+}
+
