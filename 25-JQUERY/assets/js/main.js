1e0871e4b7d68788c29c4a74a5a8f2bea533ee0c
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2022-03-28 14:26:23 +0200

    Maj du projet 25

---
+++ 25-JQUERY/assets/js/main.js
@@ -0,0 +1,25 @@
+// Crée par Joachim Zadi le 28/03/2022 à 13:41. Version 1.0
+// ========================================================
+
+// En JavaScript
+// window.addEventListener('load', function () {
+//     console.log("Document chargé");
+// });
+//
+// console.log(jQuery());
+// console.log($());
+
+// En jQuery, jQuery() & $()  designe le meme objet
+// jQuery(document).ready(function (e) {
+//     // Je met tout mon code jQuery & JS
+//     console.log("Document pret à l'emploi");
+// });
+
+// $(document).ready(function (e) {
+//     // Je met tout mon code jQuery & JS
+//     console.log("Document pret à l'emploi");
+// });
+
+
+
+// document.querySelector('p').innerText = 'Je suis un paragraphe';
